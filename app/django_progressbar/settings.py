475359b2f48a3c26a2463925b8e4c19e0b899348
ekity1002 <dreamguidance1@gmail.com>
2023-04-08 15:22:23 +0900

    add celery redis settings

--- app/django_progressbar/settings.py
+++ app/django_progressbar/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CELERY
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_REDIS_URL", default="redis://localhost:6379")
--- app/upload_file/views.py
+++ app/upload_file/views.py
@@ -3,6 +3,8 @@ import time
 import zipfile
 from io import BytesIO
 
+from celery import shared_task
+from celery_progress.backend import ProgressRecorder
 from django.core.files.storage import default_storage
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.shortcuts import render
@@ -13,23 +15,37 @@ from .forms import UploadZipFileForm
 # Create your views here.
 
 
+@shared_task(bind=True)
 def handle_zip_upload(zip_file):
     # クライアントから送信されたzipファイルをメモリ上に一時的に保存する
     buffer = BytesIO(zip_file.read())
+    # 進行状況の初期化
+    progress_recorder = ProgressRecorder(handle_zip_upload)
 
     # zipファイルを解凍して、JSONファイルを取得する
     with zipfile.ZipFile(buffer, "r") as zip_ref:
         # zipファイル内の全てのファイルを取得する
         files = zip_ref.namelist()
-        print(f"files in zip: ", files)
-        # JSONファイルを読み込んで、内容を取得する
-        for file in files:
-            # アップロードしたファイルに対してなにか重い処理：sleepで代用
+        total_files = len(files)
+        progress_recorder.set_progress(0, total_files)
+
+        print("total files: ", total_files)
+
+        # 進行状況の初期化
+        progress_recorder.set_progress(0, total_files)
+        # ファイルを読み込んで、内容を取得する
+        result = 0
+        for idx, file in enumerate(files):
+            # 重い処理
             time.sleep(1)
+            # 進行状況を更新
+            result += 1
+            progress_recorder.set_progress(idx + 1, total_files, description=f"処理数: {idx+1}/{total_files}")
 
     # 一時的に保存したzipファイルを削除する
     zip_file.close()
     default_storage.delete(zip_file.name)
+    return result
 
 
 def upload_zip_file(request):
@@ -42,10 +58,10 @@ def upload_zip_file(request):
             zip_file = request.FILES["zip_file"]
 
             # handle_zip_upload()を呼び出して、zipファイルを処理する
-            handle_zip_upload(zip_file)
+            result = handle_zip_upload(zip_file).delay(10)
 
             # 処理が完了したら、リダイレクトなど適切なレスポンスを返す
-            return render(request, "upload_file/success.html")
+            return render(request, "upload_file/upload.html", context={"form": form, "task_id": result.task_id})
     else:
         form = UploadZipFileForm()
     # GETリクエストの場合は、ファイルアップロードのフォームを表示する
